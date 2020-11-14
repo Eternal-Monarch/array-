1ff864f5a1696e5ad5cd57a2263dd3ade831da0f
Subhasish Dutta <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-11-15 04:14:16 +0600

    Create taking input form user and show them by using array.c

---
+++ taking input form user and show them by using array.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int
+main ()
+{
+  int i;
+  int arr[5];
+  int marks[100];
+  for (i = 0; i <= 4; i++)
+
+    scanf ("%d ", &arr[i]);
+  for (i = 0; i <= 4; i++)
+    printf ("%d ", arr[i]);
+
+
+
+
+  return 0;
+}
